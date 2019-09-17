d8dacd9db8c5e5e3628fc90a5fd05d9d3086b801
vuong123s <khanhvuong123s@gmail.com>
2019-09-17 14:54:17 +0700

    First commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
 var port = 3000
-
 var low = require('lowdb')
 var FileSync = require('lowdb/adapters/FileSync')
 
 var adapter = new FileSync('db.json')
 db = low(adapter)
+
+db.defaults({ users: [] })
+  .write();
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -15,8 +17,9 @@ app.listen(port, function(){
 	console.log('Server listeninng on port ' + port)
 })
 
-app.get('/products', function(req, res){
-	res.render('index',{
-		products: db.get('products').value()
+app.get('/', function(req, res){
+	res.render('index', {
+		users: db.get('users').value()
 	})
 })
+
